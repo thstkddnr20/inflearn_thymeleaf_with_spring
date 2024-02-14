4772ba3b5df2018499ba68b3237d9ccfb17144fd
thstkddnr20 <thstkddnr20@naver.com>
2024-02-14 16:41:50 +0900

    셀렉트박스 만들기

--- src/main/java/hello/itemservice/domain/item/DeliveryCode.java
+++ src/main/java/hello/itemservice/domain/item/DeliveryCode.java
@@ -14,4 +14,9 @@ public class DeliveryCode {
 
     private String code;
     private String displayName;
+
+    public DeliveryCode(String code, String displayName) {
+        this.code = code;
+        this.displayName = displayName;
+    }
 }
